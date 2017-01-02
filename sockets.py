5c4cd92976d201caf7a61c8bcf366d98201ebc80
merrua <merrua@gmail.com>
2017-01-02 21:34:53 +0000

    fixing a typo in the sockets code

--- sockets.py
+++ sockets.py
@@ -1,5 +1,5 @@
 import socket
-s = socket.socket(socket.AF.INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 4242))
 s.send('Hello, world')
 data = s.recv(1024)
